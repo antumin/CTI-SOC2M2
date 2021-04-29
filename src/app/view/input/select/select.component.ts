48c62e1c23ca330aca1482bd7c033e80d797a947
antumin <m.vielberth@gmail.com>
2021-04-29 12:46:15 +0200

    Change selection of capability level with list

--- src/app/view/input/select/select.component.ts
+++ src/app/view/input/select/select.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list/selection-list';
 import { DataService } from 'src/app/model/data.service';
 
 @Component({
@@ -10,14 +11,38 @@ export class SelectComponent implements OnInit {
   @Input()  data;
   @Input()  title;
   @Output() dataChange = new EventEmitter<number>();
+  options: number[] = [];
+  questions: {text,disabled}[] = [];
+  relevantLevels = [];
+  
 
-  constructor(public dataService: DataService) { }
-
-  ngOnInit(): void {
-    console.log(this.data);
+  constructor(public dataService: DataService) { 
+    this.relevantLevels = this.dataService.levels.slice(1,this.dataService.levels.length);
   }
 
-  onDataChange(): void{
+  ngOnInit(): void {
+    this.questions.push({disabled: false, text:this.relevantLevels[0].question});
+  }
+
+  onDataChange(event: MatSelectionListChange): void{
+    this.options.forEach((option, index) =>{
+      if(index + 1 < this.options.length){
+        this.questions[index].disabled = true;
+      }
+      if(index + 1 < this.relevantLevels.length){
+        this.questions[index + 1] = {disabled: false, text:this.relevantLevels[index + 1].question};
+      }
+    });
+    if(this.options.length == 1){
+      this.questions[0].disabled = false;
+    }
+    this.questions.forEach((question) =>{
+      if(this.questions.length > this.options.length + 1){
+        this.questions.pop();
+      }
+    });
+
+    this.data = this.options.length;
     this.dataChange.emit(this.data);
     this.dataService.dataUpdated();
   }
